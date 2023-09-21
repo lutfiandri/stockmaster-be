9c97ecbe941d18d039e4d300be71a1391c1b5465
lutfiandri <lutfiandri232@gmail.com>
2023-09-21 07:25:07 +0700

    feat(learn): question type

---
+++ models/question.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// gambar pattern
+const questionHandwriteSchema = new mongoose.Schema({
+  imageUrl: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+});
+
+// pilihan ganda
+const questionMultipleChoiceSchema = new mongoose.Schema({
+  question: String,
+  imageUrl: String,
+  options: [String],
+  trueOptionIndex: {
+    type: Number,
+    required: true,
+  },
+  optionType: {
+    type: String,
+    required: true,
+    enum: ['short-text', 'long-text', 'image'],
+  },
+});
+
+module.exports = {
+  questionHandwriteSchema,
+  questionMultipleChoiceSchema,
+};
--- models/stockPattern.js
+++ models/stockPattern.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose');
+const {
+  questionHandwriteSchema,
+  questionMultipleChoiceSchema,
+} = require('./question');
+
+// halaman learn paling awal
+const learnPageSchema = new mongoose.Schema({
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  texts: [String],
+});
+
+// schema per page
+const learnPageComposeSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ['learn', 'handwrite', 'multiple-choice'],
+  },
+  learnPage: learnPageSchema,
+  handwritePage: questionHandwriteSchema,
+  multipleChoicePage: questionMultipleChoiceSchema,
+});
 
 const stockPatternSchema = new mongoose.Schema(
   {
@@ -14,6 +43,7 @@ const stockPatternSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    learnPages: [learnPageComposeSchema],
   },
   { timestamps: true }
 );
