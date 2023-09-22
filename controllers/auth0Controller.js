68de19b2200ca494e88eff944f770aae8234d764
lutfiandri <lutfiandri232@gmail.com>
2023-09-22 16:53:03 +0700

    chore(auth0): remove result console after success

--- controllers/auth0Controller.js
+++ controllers/auth0Controller.js
@@ -14,8 +14,6 @@ const upsertUser = async (req, res) => {
     const options = { upsert: true, new: true, setDefaultsOnInsert: true };
     const result = await User.updateOne(filter, update, options);
 
-    console.log(result);
-
     return res.json({ success: true });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
