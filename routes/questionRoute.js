18eb0671d055d5ebdb43afc2a68e01585930a486
lutfiandri <lutfiandri232@gmail.com>
2023-09-24 10:24:03 +0700

    feat(question): add many question

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/questionController');
 
 router.post('/', controller.addQuestion);
+router.post('/many', controller.addManyQuestions);
 router.get('/', controller.getQuestions);
 router.get('/:id', controller.getQuestion);
 router.delete('/:id', controller.deleteQuestion);
