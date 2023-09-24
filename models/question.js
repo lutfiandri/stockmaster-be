5eb0c138173880dadfc457f12f232d61013c3fb3
lutfiandri <lutfiandri232@gmail.com>
2023-09-24 18:10:54 +0700

    fix(question): remove _id on sub schema

--- models/question.js
+++ models/question.js
@@ -1,71 +1,80 @@
 const mongoose = require('mongoose');
 
 // gambar pattern
-const questionHandwriteSchema = new mongoose.Schema({
-  imageUrl: String,
-  title: {
-    type: String,
-    required: true,
+const questionHandwriteSchema = new mongoose.Schema(
+  {
+    imageUrl: String,
+    title: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { _id: false }
+);
 
 // pilihan ganda
-const questionMultipleChoiceSchema = new mongoose.Schema({
-  question: String,
-  imageUrl: String,
-  options: [
-    {
-      id: {
-        type: String,
-        required: true,
+const questionMultipleChoiceSchema = new mongoose.Schema(
+  {
+    question: String,
+    imageUrl: String,
+    options: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+        value: {
+          type: String,
+          required: true,
+        },
       },
-      value: {
-        type: String,
-        required: true,
-      },
+    ],
+    trueOptionId: {
+      type: String,
+      required: true,
     },
-  ],
-  trueOptionId: {
-    type: String,
-    required: true,
+    optionType: {
+      type: String,
+      required: true,
+      enum: ['short-text', 'long-text', 'image'],
+    },
   },
-  optionType: {
-    type: String,
-    required: true,
-    enum: ['short-text', 'long-text', 'image'],
-  },
-});
+  { _id: false }
+);
 
 // gabungan
-const questionSchema = new mongoose.Schema({
-  pattern: {
-    type: String,
-    required: true,
-    enum: [
-      'double-top',
-      'bearish-pennant',
-      'bullish-rectangle',
-      'falling-wedge',
-      'inverse-head-and-shoulders',
-      'bullish-pennant',
-      'inverse-cup-and-handle',
-      'double-bottom',
-      'bullish-flag',
-      'bearish-rectangle',
-      'cup-and-handle',
-      'rising-wedge',
-      'head-and-shoulders',
-      'bearish-flag',
-    ],
+const questionSchema = new mongoose.Schema(
+  {
+    pattern: {
+      type: String,
+      required: true,
+      enum: [
+        'double-top',
+        'bearish-pennant',
+        'bullish-rectangle',
+        'falling-wedge',
+        'inverse-head-and-shoulders',
+        'bullish-pennant',
+        'inverse-cup-and-handle',
+        'double-bottom',
+        'bullish-flag',
+        'bearish-rectangle',
+        'cup-and-handle',
+        'rising-wedge',
+        'head-and-shoulders',
+        'bearish-flag',
+      ],
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ['handwrite', 'multiple-choice'],
+    },
+    handwrite: questionHandwriteSchema,
+    multipleChoice: questionMultipleChoiceSchema,
   },
-  type: {
-    type: String,
-    required: true,
-    enum: ['handwrite', 'multiple-choice'],
-  },
-  handwrite: questionHandwriteSchema,
-  multipleChoice: questionMultipleChoiceSchema,
-});
+  { timestamps: true }
+);
 
 const Question = mongoose.model('Question', questionSchema);
 
