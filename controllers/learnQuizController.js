a441e0210391897c124b062de8921776133cb8cb
lutfiandri <lutfiandri232@gmail.com>
2023-09-26 10:52:28 +0700

    fix(answer): fix status get last attempt to 200

--- controllers/learnQuizController.js
+++ controllers/learnQuizController.js
@@ -67,8 +67,7 @@ const getLastAttempt = async (req, res) => {
       .sort({ _id: -1 })
       .exec();
 
-    console.log(result);
-    return res.status(201).json({ success: true, data: result });
+    return res.status(200).json({ success: true, data: result });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
